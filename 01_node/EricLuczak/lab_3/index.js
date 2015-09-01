691b8d5e9e9db29c616a40365027209b3cde1ce6
Ireneusz Luczak <ericluczak@Ireneuszs-MacBook-Pro.local>
2015-09-01 16:54:31 -0500

    finished afterhours

---
+++ 01_node/EricLuczak/lab_3/index.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var fs = require('fs');
+
+var json = fs.readFileSync('./data.json');
+
+http.createServer(function (request, response) {
+  response.writeHead(200);
+  response.write(json);
+  response.end();
+}).listen(5000);
+
+console.log('Listening on 5000');
