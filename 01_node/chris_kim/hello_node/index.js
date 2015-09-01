6694e83b6beb64c6688eae82cd5b7dab5888b1a6
Christopher Kim <ckim@CKim-MacBook-Pro.local>
2015-09-01 15:59:17 -0500

    node exercises

---
+++ 01_node/chris_kim/hello_node/index.js
@@ -0,0 +1,12 @@
+// assign a variable to an NPM library
+var http = require('http');
+
+// http library: create a server and listen for requests
+// and responses
+http.createServer(function(request, response) {
+  response.writeHead(200); // status message 200 okay
+  response.write('I just built a server'); // send data to server
+  response.end(); // stop sending
+}).listen(5000);
+
+console.log('My server is up')
