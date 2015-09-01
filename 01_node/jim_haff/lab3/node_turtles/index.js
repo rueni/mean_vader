71dc03a1af2b55b2cdda21b00fa06e5e6cbd8842
James Haff <jehaff@James-Haffs-MacBook-Pro.local>
2015-09-01 16:54:58 -0500

    homework

--- 01_node/jim_haff/lab3/node_turtles/index.js
+++ 01_node/jim_haff/lab3/node_turtles/index.js
@@ -1,17 +1,14 @@
 var http = require('http');
 var fs = require('fs');
 
-var html = fs.readFileSync('./index.html')
 
-
+var data = fs.readFileSync('./data.json');
 
 http.createServer(function(request, response){
 
   response.writeHead(200);
-  response.write(html);
+  response.write(data);
   response.end();
 
-}).listen(5000);
 
-
-console.log('html server on port 5000');
+}).listen(5000)
